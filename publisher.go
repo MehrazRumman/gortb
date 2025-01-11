68ffbd2aab920bb94c3c251a1b91a2f8304764a0
MehrazRumman <stardark394@gmail.com>
2025-01-11 19:08:46 +0600

    pub added

---
+++ publisher.go
@@ -0,0 +1,8 @@
+
+type Publisher struct {
+	ID    string   `json:"id,omitempty"`
+	Name  string   `json:"name,omitempty"`
+	Cat   []string `json:"cat,omitempty"`
+	Domain string `json:"domain,omitempty"`
+	Ext   interface{} `json:"ext,omitempty"`
+}
