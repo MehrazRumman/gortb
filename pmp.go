d3d7659f98a9f1da60498c25ae449046d146ed96
MehrazRumman <stardark394@gmail.com>
2025-01-11 19:07:02 +0600

    pmp added

---
+++ pmp.go
@@ -0,0 +1,5 @@
+type PMP struct {
+	PrivateAuction int     `json:"private_auction,omitempty" default:"0"`
+	Deals          []Deal  `json:"deals,omitempty"`
+	Ext            interface{} `json:"ext,omitempty"`
+}
