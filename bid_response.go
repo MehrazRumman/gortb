406f891703c686b9788907c7660e7d87908403af
MehrazRumman <stardark394@gmail.com>
2025-01-11 19:11:49 +0600

    bid response added

---
+++ bid_response.go
@@ -0,0 +1,10 @@
+
+type BidResponse struct {
+	ID         string       `json:"id" binding:"required"`
+	SeatBids   []SeatBid    `json:"seatbid" binding:"required"`
+	BidID      string       `json:"bidid,omitempty"`
+	Cur        string       `json:"cur,omitempty" default:"USD"`
+	CustomData string       `json:"customdata,omitempty"`
+	NBR        int          `json:"nbr,omitempty"`
+	Ext        interface{}  `json:"ext,omitempty"`
+}
---
+++ segment.go
@@ -0,0 +1,6 @@
+type Segment struct {
+	ID    string      `json:"id,omitempty"`
+	Name  string      `json:"name,omitempty"`
+	Value string      `json:"value,omitempty"`
+	Ext   interface{} `json:"ext,omitempty"`
+}
