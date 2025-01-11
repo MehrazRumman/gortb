38c15f93f46913d471ded4017bb8fcd556b364a1
MehrazRumman <stardark394@gmail.com>
2025-01-11 19:12:25 +0600

    seatbid added

---
+++ seatbid.go
@@ -0,0 +1,6 @@
+type SeatBid struct {
+	Bids  []Bid        `json:"bid" binding:"required"`
+	Seat  string       `json:"seat,omitempty"`
+	Group int          `json:"group,omitempty" default:"0"`
+	Ext   interface{}  `json:"ext,omitempty"`
+}
